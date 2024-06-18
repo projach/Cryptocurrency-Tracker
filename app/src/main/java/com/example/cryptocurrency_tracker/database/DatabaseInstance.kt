db620e77bafa861894307897385fd2767deeb8dc
Iakovos Chaliasas <34036272+projach@users.noreply.github.com>
2024-06-18 21:24:57 +0300

    fixed problem with room database

--- app/src/main/java/com/example/cryptocurrency_tracker/database/DatabaseInstance.kt
+++ app/src/main/java/com/example/cryptocurrency_tracker/database/DatabaseInstance.kt
@@ -1,8 +1,9 @@
 package com.example.cryptocurrency_tracker.database
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(entities = [UserEntity::class], version = 1)
-abstract class DatabaseInstance{
+abstract class DatabaseInstance : RoomDatabase(){
     abstract fun getUserDao(): UserDao
 }

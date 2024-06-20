1688110781a4e5a3350f36a3e09ba5412c6e1e2b
Iakovos Chaliasas <34036272+projach@users.noreply.github.com>
2024-06-20 17:53:19 +0300

    Now the main fragment works correctly when offline

--- app/src/main/java/com/example/cryptocurrency_tracker/recyclerview/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/cryptocurrency_tracker/recyclerview/RecyclerViewAdapter.kt
@@ -1,13 +1,14 @@
 package com.example.cryptocurrency_tracker.recyclerview
 
-import android.util.Log
-import android.view.ViewGroup
 import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.example.cryptocurrency_tracker.database.UserEntity
 import com.example.cryptocurrency_tracker.databinding.CoinViewBinding
 import com.example.cryptocurrency_tracker.network.JsonView
 
-class RecyclerViewAdapter(private val arrayData: Array<JsonView>,private val arrayID: List<Int>) : RecyclerView.Adapter<RecyclerViewHolder>() {
+class RecyclerViewAdapter(private val arrayData: List<UserEntity>,private val fragment: Fragment) : RecyclerView.Adapter<RecyclerViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
         val view =
             CoinViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
--- app/src/main/java/com/example/cryptocurrency_tracker/recyclerview/RecyclerViewHolder.kt
+++ app/src/main/java/com/example/cryptocurrency_tracker/recyclerview/RecyclerViewHolder.kt
@@ -2,22 +2,24 @@ package com.example.cryptocurrency_tracker.recyclerview
 
 import android.util.Log
 import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.cryptocurrency_tracker.database.UserEntity
 import com.example.cryptocurrency_tracker.databinding.CoinViewBinding
 import com.example.cryptocurrency_tracker.fragments.MainScreenFragment
 import com.example.cryptocurrency_tracker.network.JsonView
 import com.squareup.picasso.Picasso
 
 class RecyclerViewHolder(val binding: CoinViewBinding): RecyclerView.ViewHolder(binding.root) {
-    fun bind(data: JsonView){
+    fun bind(data: UserEntity, fragment: Fragment){
         val imageV: ImageView = binding.viewImageCoin
         Picasso.get().load(data.image).into(imageV)
         binding.viewTextPrice.text = data.currentPrice.toString().plus("€")
-        binding.viewTextId.text = data.id
+        binding.viewTextId.text = data.id.toString()
         binding.viewTextSymbol.text = data.symbol
         binding.constraintLayoutCoin.setOnClickListener{
-//      TODO("Add button functionality to send over to details")
+//            TODO("Add button functionality to send over to details")
         }
     }
 }

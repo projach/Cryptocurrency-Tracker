1730be4af0ed6ba89be609e25926a16a5f06f7e6
Iakovos Chaliasas <34036272+projach@users.noreply.github.com>
2024-06-20 18:16:51 +0300

    from databases branch

--- app/src/main/java/com/example/cryptocurrency_tracker/recyclerview/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/cryptocurrency_tracker/recyclerview/RecyclerViewAdapter.kt
@@ -1,13 +1,12 @@
 package com.example.cryptocurrency_tracker.recyclerview
 
-import android.util.Log
-import android.view.ViewGroup
 import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.cryptocurrency_tracker.network.JsonView
+import com.example.cryptocurrency_tracker.database.UserEntity
 import com.example.cryptocurrency_tracker.databinding.CoinViewBinding
 
-class RecyclerViewAdapter(private val arrayData: Array<JsonView>,private val arrayID: List<Int>) : RecyclerView.Adapter<RecyclerViewHolder>() {
+class RecyclerViewAdapter(private val arrayData: List<UserEntity>) : RecyclerView.Adapter<RecyclerViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
         val view =
             CoinViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -17,6 +16,6 @@ class RecyclerViewAdapter(private val arrayData: Array<JsonView>,private val arr
     override fun getItemCount(): Int = arrayData.size
 
     override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
-        holder.bind(arrayData[position], arrayID[position])
+        holder.bind(arrayData[position])
     }
 }

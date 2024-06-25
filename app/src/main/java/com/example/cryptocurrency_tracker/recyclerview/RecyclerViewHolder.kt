fe7ebf7815c0edc231e313e061cc98919285ac5c
Iakovos Chaliasas <34036272+projach@users.noreply.github.com>
2024-06-25 14:18:22 +0300

    Changed the recycler view UI to be more minimal

--- app/src/main/java/com/example/cryptocurrency_tracker/recyclerview/RecyclerViewHolder.kt
+++ app/src/main/java/com/example/cryptocurrency_tracker/recyclerview/RecyclerViewHolder.kt
@@ -6,6 +6,7 @@ import com.example.cryptocurrency_tracker.R
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cryptocurrency_tracker.database.UserEntity
 import com.example.cryptocurrency_tracker.databinding.CoinViewBinding
+import java.util.Locale
 
 class RecyclerViewHolder(
     val binding: CoinViewBinding,
@@ -16,15 +17,14 @@ class RecyclerViewHolder(
     fun bind(data: UserEntity){
         val imageV: ImageView = binding.viewImageCoin
         Picasso.get().load(data.image).into(imageV)
-        binding.viewTextPrice.text = data.currentPrice.toString().plus("€")
-        binding.viewTextId.text = data.id.toString()
+        binding.viewTextPrice.text = String.format(Locale.getDefault(),"%.2f", data.currentPrice).plus("€")
         binding.viewTextSymbol.text = data.symbol
 
 //        binding.constraintLayoutCoin.setOnClickListener{
 ////      TODO: Add button functionality to send over to details ?
 //        }
 
-        binding.displayBtn.setOnClickListener {
+        binding.constraintLayoutCoin.setOnClickListener {
             onDisplayClick(data)
         }
 
